7060557124d393ad7c61b7ad7ba9839c5e338250
Angus <angus.goody@gmail.com>
2025-02-06 23:24:20 +0000

    pagination

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 from dashboards.access_control import has_dashboard_access
 from dashboards.dependencies import get_dataset_service
@@ -8,15 +9,19 @@ from dashboards.dependencies import get_dataset_service
 @login_required
 @user_passes_test(lambda u: has_dashboard_access(u, "dataset_dashboard"))
 def dataset_dashboard(request):
-
     # Load the dataset service
     dataset_service = get_dataset_service()
 
     # Fetch all the datasets
     datasets = dataset_service.get_all_datasets()
 
-    # Pass the datasets to the template
-    return render(request, "dashboards/datasets.html", {"datasets": datasets})
+    # Paginate datasets (e.g., 10 datasets per page)
+    paginator = Paginator(datasets, 10)  # Adjust number per page as needed
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    # Pass the paginated object to the template
+    return render(request, "dashboards/datasets.html", {"dataset_page": page_obj})
 
 
 @login_required
