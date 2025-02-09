266141819a6fa51b4a8c2bd90ddfedd8c4fb6454
Angus <angus.goody@gmail.com>
2025-02-09 00:21:48 +0000

    updated UI form password reset

--- baddy/settings.py
+++ baddy/settings.py
@@ -52,7 +52,8 @@ STATICFILES_FINDERS = [
 ]
 
 # Email settings (Currently set to console backend)
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
 
 # Application definition
 
