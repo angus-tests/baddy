6a944898aad1a9d439a4a07bdb8fcc265d8507d4
Angus Goody <angus.goody@gmail.com>
2025-02-15 21:00:25 +0000

    Improved search (#9)
    * clean up
    
    * better search

--- dashboards/models/sds/dataset.py
+++ dashboards/models/sds/dataset.py
@@ -17,13 +17,22 @@ class Dataset:
     def __str__(self):
         return self.dataset_id
 
-    def _get_date_string(self):
+    def _get_date_string(self, date_object: date = None):
         """
         Get a string representation of the date.
         """
-        if self.published_at:
-            # Format into long date
-            return self.published_at.strftime("%B %d, %Y")
+        if date_object:
+            # Create multiple date formats to search
+            date_formats = [
+                "%Y-%m-%",
+                "%y-%m-%d",
+                "%Y/%m/%",
+                "%y/%m/%d",
+            ]
+            date_string = ""
+            for date_format in date_formats:
+                date_string += f"{date_object.strftime(date_format)} "
+            return date_string
         return ""
 
     def get_search_string(self):
@@ -35,8 +44,14 @@ class Dataset:
         date_values = [self.published_at] if self.published_at else []
 
         # Create a string representation of the dataset
-        return (" ".join(
+        main_string = (" ".join(
             [
                 str(val).lower() for val in self.__dict__.values() if val and val not in date_values
             ]
-        ) + self._get_date_string()).lower()
+        ))
+
+        # Add the date values
+        date_strings = [self._get_date_string(date_val) for date_val in date_values]
+
+        # Add the date strings to the main string
+        return main_string + " ".join(date_strings)
