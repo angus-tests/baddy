ce3249757330dffcebb1ec2d839369c7834147c4
Angus <angus.goody@ons.gov.uk>
2025-02-18 15:04:24 +0000

    consistent

--- baddy/context_processors.py
+++ baddy/context_processors.py
@@ -24,8 +24,11 @@ def production_flag(request):
 def get_vite_asset(file_name):
     """Reads Vite manifest to get the hashed asset file."""
     manifest_path = os.path.join(settings.BASE_DIR, 'static', 'dist', '.vite/manifest.json')
-    with open(manifest_path, 'r') as manifest_file:
-        manifest = json.load(manifest_file)
+    try:
+        with open(manifest_path, 'r') as manifest_file:
+            manifest = json.load(manifest_file)
+    except FileNotFoundError:
+        return file_name
 
     # The `file_name` should be the original name without hash
     return manifest.get(file_name, {}).get('file', file_name)
